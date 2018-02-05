e30840770669138b4dd120a0eca6ed5145376519
Nathan <nathan.hsu@icloud.com>
2018-02-04 22:19:24 -0800

    It builds and runs
    Find largest number in array

--- Largest Number/main.m
+++ Largest Number/main.m
@@ -8,10 +8,35 @@
 
 #import <Foundation/Foundation.h>
 
+
+id findLargestNumberInArray(NSArray *array) {
+    
+    id largestNumber = array[0];
+    NSUInteger arraySize = array.count;
+    for (int i = 1; i < arraySize; i++) {
+        if (array[i] > largestNumber) {
+            largestNumber = array[i];
+        }
+    }
+    return largestNumber;
+    
+}
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        NSArray *myArray = @[@3, @7, @6, @8];
+        NSArray *secondArray = @[@44, @5, @6];
+        
+        id largestNumInMyArray = findLargestNumberInArray(myArray);
+        NSLog(@"Largest number in the first array is: %@", largestNumInMyArray);
+        
+        
+        id largestNumInSecondArray = findLargestNumberInArray(secondArray);
+        NSLog(@"Largest number in the second array is: %@", largestNumInSecondArray);
+        
+        
     }
     return 0;
 }
